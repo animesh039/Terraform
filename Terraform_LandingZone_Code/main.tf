f267f6a6d8fe42a86c03f57022e3f016ebc333b2
animesh039 <119167093+animesh039@users.noreply.github.com>
2026-08-09 18:52:12 +0530

    Update main.tf

--- Terraform_LandingZone_Code/main.tf
+++ Terraform_LandingZone_Code/main.tf
@@ -2,21 +2,33 @@ resource "azurerm_resource_group" "rg-hub" {
   name = "rg-azt-hub-weu-01"
   location = "westeurope"
   tags = {
-    "DeploymentDate" = "14052025"
+    DeploymentDate = timestamp()
+    DeployedBy = "Terraform"
+  }
+  lifecycle {
+    ignore_changes = [ tags["DeploymentDate"] ]
   }
 }
 resource "azurerm_resource_group" "rg-tst" {
   name = "rg-azt-tst-weu-01"
   location = "westeurope"
   tags = {
-    "DeploymentDate" = "14052025"
+    DeploymentDate = timestamp()
+    DeployedBy = "Terraform"
+  }
+  lifecycle {
+    ignore_changes = [ tags["DeploymentDate"] ]
   }
 }
 resource "azurerm_resource_group" "rg-prd" {
   name = "rg-azt-prd-weu-01"
   location = "westeurope"
   tags = {
-    "DeploymentDate" = "14052025"
+    DeploymentDate = timestamp()
+    DeployedBy = "Terraform"
+  }
+  lifecycle {
+    ignore_changes = [ tags["DeploymentDate"] ]
   }
 }
 resource "azurerm_network_security_group" "nsg-db-tst" {
