7558bb8bec4e6073c86b0fb8d1f10873031d56a1
animesh039 <119167093+animesh039@users.noreply.github.com>
2026-08-09 17:35:43 +0530

    Update main.tf

--- Terraform_LandingZone_Code/main.tf
+++ Terraform_LandingZone_Code/main.tf
@@ -19,3 +19,39 @@ resource "azurerm_resource_group" "rg-prd" {
     "DeploymentDate" = "14052025"
   }
 }
+resource "azurerm_network_security_group" "nsg-db-tst" {
+  name = "nsg-db-tst-weu-01"
+  location = azurerm_resource_group.rg-tst.location
+  resource_group_name = azurerm_resource_group.rg-tst.name
+}
+resource "azurerm_network_security_group" "nsg-be-tst" {
+  name = "nsg-be-tst-weu-01"
+  location = azurerm_resource_group.rg-tst.location
+  resource_group_name = azurerm_resource_group.rg-tst.name
+}
+resource "azurerm_network_security_group" "nsg-fe-tst" {
+  name = "nsg-fe-tst-weu-01"
+  location = azurerm_resource_group.rg-tst.location
+  resource_group_name = azurerm_resource_group.rg-tst.name
+}
+resouece "azurerm_virtual_network" "vnet-tst" {
+  name = "vnet-azt-tst-weu-01"
+  location = azurerm_resource_group.rg-tst.location
+  resource_group_name = azurerm_resource_group.rg-tst.name
+  address_space = ["172.18.0.0/24"]
+  subnet {
+    name = "snet-db-tst-weu-01"
+    address_prefixes = ["172.18.0.0/28"]
+    security_group = azurerm_network_security_group.nsg-db-tst.id
+  }
+  subnet {
+    name = "snet-be-tst-weu-01"
+    address_prefixes = ["172.18.0.16/28"]
+    security_group = azurerm_network_security_group.nsg-be-tst.id
+  }
+  subnet {
+    name = "snet-fe-tst-weu-01"
+    address_prefixes = ["172.18.0.48/28"]
+    security_group = azurerm_network_security_group.nsg-fe-tst.id
+  }
+}
