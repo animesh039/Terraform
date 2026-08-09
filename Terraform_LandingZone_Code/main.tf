1512c14a6aa0c8bd04c9c6c4dc8f7e0a8e1b058b
animesh039 <119167093+animesh039@users.noreply.github.com>
2026-08-09 16:48:15 +0530

    Update main.tf

--- Terraform_LandingZone_Code/main.tf
+++ Terraform_LandingZone_Code/main.tf
@@ -1,1 +1,23 @@
-
+``` hcl
+resource "azurerm_resource_group" "rg-hub" {
+  name = "rg-azt-hub-weu-01"
+  location = "westeurope"
+  tags = {
+    "DeploymentDate" = "14052025"
+  }
+}
+resource "azurerm_resource_group" "rg-tst" {
+  name = "rg-azt-tst-weu-01"
+  location = "westeurope"
+  tags = {
+    "DeploymentDate" = "14052025"
+  }
+}
+resource "azurerm_resource_group" "rg-prd" {
+  name = "rg-azt-prd-weu-01"
+  location = "westeurope"
+  tags = {
+    "DeploymentDate" = "14052025"
+  }
+}
+```
