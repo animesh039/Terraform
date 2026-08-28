605643ead6ca7a5dee8b3f0daa39cbd1cba04c85
animesh039 <119167093+animesh039@users.noreply.github.com>
2026-08-29 01:21:45 +0530

    Add files via upload

---
+++ Terraform_Modules/modules/azurerm_resource_group/output.tf
@@ -0,0 +1,7 @@
+output "output_resource_group_name" {
+  value = azurerm_resource_group.resource_group.name
+}
+
+output "output_resource_group_location" {
+  value = azurerm_resource_group.resource_group.location
+}
